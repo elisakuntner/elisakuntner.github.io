8f08bea29cac6c44539a67c73cef7d3555eb3f0c
Elisa Kuntner <elisa.kuntner@student.uibk.ac.at>
2021-03-25 16:31:30 +0100

    stop element

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,16 @@
 // console.log("Hello World");
 // console.log(L);
+let stop = {
+    nr: 18,
+    name: "Abel Tasman National Park",
+    lat: -40.50,
+    long: 172.54,
+    user: "elisakuntner",
+    wikipedia: "https://en.wikipedia.org/wiki/Abel_Tasman_National_Parkhttps://en.wikipedia.org/wiki/Abel_Tasman_National_Park"
+
+}
+
+
 const map = L.map("map",{
     center: [ -40.50, 172.54 ], //coordinaten einfügen
     zoom: 13,
