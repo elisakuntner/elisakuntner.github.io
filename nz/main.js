2600a9f1514e47652d6bce9b2125a03fe7255ce8
Elisa Kuntner <elisa.kuntner@student.uibk.ac.at>
2021-03-25 16:13:24 +0100

    karte in js

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,12 @@
-console.log("Hello World");
-console.log(L);
+// console.log("Hello World");
+// console.log(L);
+const map = L.map("map"), {
+    center: [ -40.50, 172.54 ], //coordinaten einfügen
+    zoom: 13
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org{z}/{x}/{y}.png")
+    ]
 
-const map = L.map("map");
+};
 console.log(document.querySelector("#map"))
+//Abel-Tasman-Nationalpark
