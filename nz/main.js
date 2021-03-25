fafd82b55b2cc34a43a9c3ff19438b9b1a5f5b4e
Elisa Kuntner <elisa.kuntner@student.uibk.ac.at>
2021-03-25 17:36:05 +0100

    fehler

--- nz/main.js
+++ nz/main.js
@@ -24,7 +24,7 @@ for (let entry of ROUTE){
     let mrk = L.marker([entry.lat, entry.lng ]).addTo(map);
     mrk.bindPopup(`
         <h4>Stop ${entry.nr}: ${entry.name}</h4>
-        <p><a href="${entry.wikipedia}">i class="fas fa-external-link-alt mr-3"></i>Read about stop in Wikipedida</a></p>
+        <p><a href="${entry.wikipedia}">i class="fas fa-external-link-alt mr-3"></i>Read about stop in Wikipedia</a></p>
         `); //mit diesen marken kann man gut übersichtlich arbeiten.. aber ohne autocomplet, kann man ev ausserhalb von den bakc ``machen.
         
         if (CustomElementRegistry.nr == 18) {
