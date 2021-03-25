9ad53ea3602847a77f53c7b66d18bd3508928a58
Elisa Kuntner <elisa.kuntner@student.uibk.ac.at>
2021-03-25 16:20:16 +0100

    main.js

--- nz/main.js
+++ nz/main.js
@@ -2,11 +2,14 @@
 // console.log(L);
 const map = L.map("map"), {
     center: [ -40.50, 172.54 ], //coordinaten einfügen
-    zoom: 13
+    zoom: 13,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org{z}/{x}/{y}.png")
     ]
+});
 
-};
-console.log(document.querySelector("#map"))
+let mrk = L.marker([-40.50, 172.54 ]); add
+
+
+// console.log(document.querySelector("#map"))
 //Abel-Tasman-Nationalpark
