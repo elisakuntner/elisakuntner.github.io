c27f163ba65dba011f06913c2ce4321c81140410
Elisa Kuntner <elisa.kuntner@student.uibk.ac.at>
2021-04-21 15:29:36 +0200

    schneehöhen farben

--- aws-tirol/colors.js
+++ aws-tirol/colors.js
@@ -28,10 +28,75 @@ const COLORS = {
         max: 999,
         col: "#ff3232"
     }],
-    temperature: [
+    temperature: [{
+        min: 0,
+        max: 5,
+        col: "#ffff64"
+    }, {
+        min: 5,
+        max: 10,
+        col: "#c8ff64"
+    }, {
+        min: 10,
+        max: 20,
+        col: "#96ff96"
+    }, {
+        min: 20,
+        max: 40,
+        col: "#32c8ff"
+    }, {
+        min: 40,
+        max: 60,
+        col: "#6496ff"
+    }, {
+        min: 60,
+        max: 80,
+        col: "#9664ff"
+    }, {
+        min: 80,
+        max: 999,
+        col: "#ff3232"
+    }
+        
 
     ],
-    snowheight: [
+    snowheight: [{
+        min: 0,
+        max: 1,
+        col: "#fff"
+    }, {    
+        min: 1,
+        max: 10,
+        col: "#ffffb2"
+    }, {
+        min: 10,
+        max: 25,
+        col: "#b0ffbc"
+    }, {
+        min: 25,
+        max: 50,
+        col: "#8cffff"
+    }, {
+        min: 50,
+        max: 100,
+        col: "#19cdff"
+    }, {
+        min: 100,
+        max: 200,
+        col: "#1982ff"
+    }, {
+        min: 200,
+        max: 300,
+        col: "#0f5abe"
+    }, {
+        min: 300,
+        max: 400,
+        col: "#784bff"
+    }, {
+        min: 400,
+        max: >400,
+        col: "##cd0feb"
+    }
 
     ]
 };
