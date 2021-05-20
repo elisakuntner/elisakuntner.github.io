086370214acfc2cb9f63eab692953646fc0b95ed
Elisa Kuntner <elisa.kuntner@student.uibk.ac.at>
2021-05-20 16:01:03 +0200

    ordner tirol erstellen

---
+++ tirol/main.js
@@ -0,0 +1,61 @@
+/* global L */
+// Bike Trail Tirol Beispiel
+
+// Kartenhintergründe der basemap.at definieren
+let baselayers = {
+    standard: L.tileLayer.provider("BasemapAT.basemap"),
+    grau: L.tileLayer.provider("BasemapAT.grau"),
+    terrain: L.tileLayer.provider("BasemapAT.terrain"),
+    surface: L.tileLayer.provider("BasemapAT.surface"),
+    highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
+    ortho_overlay: L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ]),
+};
+
+// Overlays für die Themen zum Ein- und Ausschalten definieren
+let overlays = {
+    tracks: L.featureGroup()
+};
+
+// Karte initialisieren und auf Innsbrucks Wikipedia Koordinate blicken
+let map = L.map("map", {
+    fullscreenControl: true,
+    center: [47.267222, 11.392778],
+    zoom: 9,
+    layers: [
+        baselayers.grau
+    ]
+})
+// Kartenhintergründe und Overlays zur Layer-Control hinzufügen
+let layerControl = L.control.layers({
+    "basemap.at Standard": baselayers.standard,
+    "basemap.at grau": baselayers.grau,
+    "basemap.at Relief": baselayers.terrain,
+    "basemap.at Oberfläche": baselayers.surface,
+    "basemap.at hochauflösend": baselayers.highdpi,
+    "basemap.at Orthofoto beschriftet": baselayers.ortho_overlay
+}, {
+    "GPX-Tracks": overlays.tracks
+}).addTo(map);
+
+// Overlay mit GPX-Track anzeigen
+overlays.tracks.addTo(map);
+
+const drawTrack = () => {
+    console.log("Track: ", nr);
+    let gpxTrack = new L.GPX(`tracks/${nr}.gpx`, {
+        async: true, //datei aus inet geladen, wartet mit dem bis es komplett geladen ist über server
+        marker_options: {
+            startIconUrl: `icons/number_${nr}.png`,
+            endIconUrl: 'iconss/finish.png',
+            shadowUrl: null, //wird mit null nicht angezeigt
+        }
+    }).addTo(overlays.tracks); //Var definieren u auf den ordner tracks zugreifen
+};
+
+const seectedTrack = 31;
+drawTrack(selectedTrack);
+
+//https://github.com/mpetazzoni/leaflet-gpx Leafletplugin für GPX dateien
